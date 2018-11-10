c8f552f12c21f1b5a265f3aa02368b7ed5018672
Rashid <34310411+frankenstein32@users.noreply.github.com>
2018-11-10 16:55:09 +0530

    files added

---
+++ TTClient.java
@@ -0,0 +1,17 @@
+package Game;
+
+/**
+ * @author RASHID
+ *
+ * @date Jan 27, 2018
+ */
+
+public class TTClient {
+
+	public static void main(String[] args) {
+
+		TypingTutor tt = new TypingTutor(args);
+
+	}
+
+}
